25dc7a6620aac5e63eff8bb84fb3272d191568ae
林颖 <linying@i-counting.cn>
2019-08-24 17:24:52 +0800

    [feat] first 搭建 路由 related to issue #1

---
+++ src/components/GamesPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class GamesPage extends React.Component {
+  render () {
+    return (
+      <div>
+        GamesPage
+      </div>
+    )
+  }
+}
+
+export default GamesPage
--- src/index.js
+++ src/index.js
@@ -1,10 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './reducers'
+/**redux工具调试 */
+import { composeWithDevTools } from 'redux-devtools-extension'
+import logger from 'redux-logger'
+
+/**路由 */
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
+
+/**组件 */
+import GamesPage from './components/GamesPage'
+
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(logger)
+  )
+)
+
+ReactDOM.render(
+  <Provider store={ store }>
+    <Router>
+      <div className='ui container'>
+        <div className='ui three item menu'>
+          <NavLink exact activeClassName='active' className='item' to='/'>Home</NavLink>
+          <NavLink activeClassName='active' className='item' to='/games'>Games</NavLink>
+          <NavLink activeClassName='active' className='item' to='/games/new'>Add New Game</NavLink>
+        </div>
+        <Route exact path='/' component={ App } />
+        <Route path='/games' component={ GamesPage } />
+      </div>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
